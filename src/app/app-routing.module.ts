404a0b7584beb18e1f6c4f47d987e2369b847b97
Sebastian Jimenez <jimenez1121monroy@gmail.com>
2024-05-27 21:06:28 -0500

    organismos login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppointmentFormComponent } from './shared/components/molecules/appointment-form/appointment-form.component';
-import { ButtonSetLoginRegistrationComponent } from './shared/components/molecules/button-set-login-registration/button-set-login-registration.component';
-import { ImageCardServicesComponent } from './shared/components/molecules/image-card-services/image-card-services.component';
+import { RegisterComponent } from './shared/components/organisms/register/register.component';
 
 const routes: Routes = [
-    {path:'formulario', component: AppointmentFormComponent},
-    {path:'login', component: ButtonSetLoginRegistrationComponent},
-    {path:'calendario', component: ImageCardServicesComponent }
+    {path:'header', component: RegisterComponent}
 ];
 
 @NgModule({
