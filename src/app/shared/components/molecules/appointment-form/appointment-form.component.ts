bf52c763127f505dad749a9a575a3d208dcb2c2e
Sebastian Jimenez <jimenez1121monroy@gmail.com>
2024-06-11 09:33:22 -0500

    creacion del stepper completlo

--- src/app/shared/components/molecules/appointment-form/appointment-form.component.ts
+++ src/app/shared/components/molecules/appointment-form/appointment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Patient } from '../../../../core/models/patient.model';
 
@@ -8,22 +8,36 @@ import { Patient } from '../../../../core/models/patient.model';
   styleUrls: ['./appointment-form.component.css']
 })
 export class AppointmentFormComponent implements OnInit {
-  @Input() appointmentData!: Patient;
-  @Output() formDataReady = new EventEmitter<any>(); // Evento para emitir los datos del formulario
+  @Input() appointmentData!: Patient; // Recibe los datos del paciente
 
   appointmentForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // Configurar el formulario
     this.appointmentForm = this.fb.group({
-      // Definir los campos del formulario
+      nombre: [''],
+      apellido: [''],
+      documento: [''],
+      email: [''],
+      telefono: [''],
+      motivo: ['']
     });
+
+    // Si hay datos de paciente, actualizar el formulario
+    if (this.appointmentData) {
+      this.appointmentForm.patchValue({
+        nombre: this.appointmentData.nombre,
+        apellido: this.appointmentData.apellido,
+        documento: this.appointmentData.documento,
+        email: this.appointmentData.email,
+        telefono: this.appointmentData.telefono
+      });
+    }
   }
 
   onSubmit() {
-    // Emitir los datos del formulario cuando se envíe
-    this.formDataReady.emit(this.appointmentForm.value);
+    console.log(this.appointmentForm.value);
+    // Aquí puedes emitir un evento para enviar los datos al componente del stepper
   }
 }
